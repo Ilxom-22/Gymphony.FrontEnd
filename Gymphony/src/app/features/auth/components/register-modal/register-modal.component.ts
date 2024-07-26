bd6808f04c84ff6eeee1d3d849e6f85553209683
Ilxom <ilxomkarimjonov22@gmail.com>
2024-07-26 15:47:19 +0500

    Add register modal component

--- Gymphony/src/app/features/auth/components/register-modal/register-modal.component.ts
+++ Gymphony/src/app/features/auth/components/register-modal/register-modal.component.ts
@@ -3,42 +3,44 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { SignUpDetails } from '../../interfaces/sign-up-details.interface';
 import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
-import { User } from '../../../../core/interfaces/user';
-
+import { alphabeticValidator } from '../../../../shared/validators/alphabetic-validator';
+import { passwordValidator } from '../../../../shared/validators/password-validator';
+import { ModalService } from '../../services/modal.service';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  selector: 'app-register-modal',
+  templateUrl: './register-modal.component.html',
+  styleUrl: './register-modal.component.css'
 })
-export class RegisterComponent {
-
+export class RegisterModalComponent {
   passwordMatchValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+
     const formGroup = control as FormGroup;
     const password = formGroup.get('authData')?.value;
     const confirmPassword = formGroup.get('confirmPassword')?.value;
-    console.log(password === confirmPassword);
     return password === confirmPassword ? null : { mismatch: true };
   };
 
   registerForm = new FormGroup({
-    firstName: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(64)]),
-    lastName: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(64)]),
+    firstName: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(64), alphabeticValidator()]),
+    lastName: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(64), alphabeticValidator()]),
     emailAddress: new FormControl('', [Validators.required, Validators.email]),
-    authData: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(32)]),
+    authData: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(32), passwordValidator()]),
     confirmPassword: new FormControl('', [Validators.required])},
     { validators: this.passwordMatchValidator }
   );
 
-  user?: User;
-
-  constructor(private authService: AuthService) { }
+  constructor(private modalService: ModalService, private authService: AuthService) { }
 
   onSubmit(): void {
     if (this.registerForm.valid) {
       const signUpDetails = this.registerForm.value as SignUpDetails;
-      this.authService.signUp(signUpDetails).subscribe(response => this.user = response);
+      this.authService.signUp(signUpDetails).subscribe(response => response);
     }
   }
-}
 
+  openLoginModal() {
+    this.modalService.showLoginModal();
+  }
+
+}
