abbe950ff88ad680d3271c3a7a38e2630c005f4f
Ilxom <ilxomkarimjonov22@gmail.com>
2024-08-07 09:00:14 +0500

    Create user information component

---
+++ Gymphony/src/app/features/user-profile/components/user-information/user-information.component.ts
@@ -0,0 +1,40 @@
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { tap } from 'rxjs';
+
+import { User } from '../../../../core/interfaces/user';
+import { AuthService } from '../../../auth/services/auth.service';
+import { ModalService } from '../../../auth/services/modal.service';
+
+@Component({
+  selector: 'app-user-information',
+  templateUrl: './user-information.component.html',
+  styleUrl: './user-information.component.css'
+})
+export class UserInformationComponent {
+  @Input() user!: User;
+
+  constructor(
+    private authService: AuthService, 
+    private router: Router, 
+    private modalService: ModalService) { }
+
+  public logOut(): void {
+
+    const dialogRef = this.modalService.showConfirmationModal('Are you sure you want to log out?', 'Logout Confirmation');
+
+    dialogRef.afterClosed()
+      .pipe(tap((result: boolean) => {
+          if (result) {
+            this.authService.logout()
+              .pipe(tap(() => this.router.navigate(['/home'])))
+              .subscribe();
+          }
+        })
+      ).subscribe();
+  }
+
+  public openChangePasswordDialog(): void {
+    this.modalService.showChangePasswordModal();
+  }
+}
