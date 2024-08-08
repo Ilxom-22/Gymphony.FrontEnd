560385819fa62dc3d30247a18d30e9d1de3803af
Ilxom <ilxomkarimjonov22@gmail.com>
2024-08-08 11:06:48 +0500

    Fix user-profile resolver

--- Gymphony/src/app/app-routing.module.ts
+++ Gymphony/src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) },
   { path: 'auth', loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule) },
-  { path: 'profile', loadChildren: () => import('./features/user-profile/user-profile.module').then(m => m.UserProfileModule), resolve: { 'profile': userProfileResolver } }
+  { path: 'profile', loadChildren: () => import('./features/user-profile/user-profile.module').then(m => m.UserProfileModule) }
 ];
 
 @NgModule({
