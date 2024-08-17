26c4fa3924399057ad010f18761874344025cdb0
Ilxom <ilxomkarimjonov22@gmail.com>
2024-08-17 11:21:26 +0500

    Create membership plan details component

---
+++ Gymphony/src/app/features/membership-plans/components/membership-plan-details-modal/membership-plan-details-modal.component.ts
@@ -0,0 +1,21 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+import { ConfirmationModalComponent } from '../../../../shared/components/confirmation-modal/confirmation-modal.component';
+import { MembershipPlanDetails } from '../../interfaces/membership-plan-details';
+
+@Component({
+  selector: 'app-membership-plan-details-modal',
+  templateUrl: './membership-plan-details-modal.component.html',
+  styleUrl: './membership-plan-details-modal.component.css'
+})
+export class MembershipPlanDetailsModalComponent {
+  public plan: MembershipPlanDetails;
+
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmationModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: MembershipPlanDetails)
+  {
+      this.plan = data;
+  }
+}
