4de596676cab30d3adeb53740688aa7bb5372ab8
Ilxom <ilxomkarimjonov22@gmail.com>
2024-08-17 16:03:15 +0500

    Refactor app component and app module

--- Gymphony/src/app/app.component.ts
+++ Gymphony/src/app/app.component.ts
@@ -1,18 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from './features/auth/services/auth.service';
+import { Component, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { User } from './core/interfaces/user';
+import { UserService } from './core/services/user.service';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit {
-  
-  constructor(
-    private authService: AuthService) { }
-
-  public ngOnInit(): void {
-    this.authService.autoLogIn().subscribe();
-  }
+export class AppComponent {
+  private userService = inject(UserService);
+  public user$: Observable<User | null> = this.userService.user$;
 }
--- Gymphony/src/app/app.module.ts
+++ Gymphony/src/app/app.module.ts
@@ -1,7 +1,8 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +14,12 @@ import { ProfileIconComponent } from './core/components/profile-icon/profile-ico
 import { AuthInterceptor } from './core/interceptors/auth.interceptor';
 import { AuthResponseInterceptor } from './core/interceptors/auth-response.interceptor';
 import { SidenavComponent } from './core/components/sidenav/sidenav.component';
+import { LoaderInterceptor } from './core/interceptors/loader.interceptor';
+import { AppInitializerService } from './core/services/app-initializer.service';
+
+export function initializeApp(appInitializerService: AppInitializerService) {
+  return () => appInitializerService.initializeApp();
+}
 
 @NgModule({
   declarations: [
@@ -28,6 +35,7 @@ import { SidenavComponent } from './core/components/sidenav/sidenav.component';
     AuthModule,
     SharedModule,
     HomeModule,
+    NgxSpinnerModule
   ],
   providers: [
     provideAnimationsAsync(),
@@ -39,6 +47,17 @@ import { SidenavComponent } from './core/components/sidenav/sidenav.component';
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthResponseInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoaderInterceptor,
+      multi: true
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeApp,
+      deps: [AppInitializerService],
       multi: true
     }
   ],
