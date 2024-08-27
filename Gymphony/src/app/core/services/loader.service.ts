1c7eeaf48cc77d387f4d84267dc49e7f93ba204b
Ilxom <ilxomkarimjonov22@gmail.com>
2024-08-27 06:33:06 +0500

    Enhance loader service and update app component

--- Gymphony/src/app/core/services/loader.service.ts
+++ Gymphony/src/app/core/services/loader.service.ts
@@ -1,32 +1,32 @@
 import { Injectable } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoaderService {
-  public requestsCount: number = 0;
+  private isLoaderShownSubject = new BehaviorSubject<boolean>(false);
+  public isLoaderShown$ = this.isLoaderShownSubject.asObservable();
 
   constructor(private spinnerService: NgxSpinnerService) { }
 
-  public busy(): void {
-    this.requestsCount++;
-
+  public show(): void {
     this.spinnerService.show(undefined, 
-    {
-      type: 'square-jelly-box',
-      bdColor: '#232429',
-      color: '#f5f5f5',
-      size: 'default'
-    });
+      {
+        type: 'square-jelly-box',
+        bdColor: '#232429',
+        color: '#f5f5f5',
+        size: 'default'
+      });
+
+    this.isLoaderShownSubject.next(true);
   }
 
-  public idle(): void {
-    this.requestsCount--;
-
-    if (this.requestsCount <= 0) {
-      this.requestsCount = 0;
+  public hide(): void {
+    setTimeout(() => {
       this.spinnerService.hide();
-    }
+      this.isLoaderShownSubject.next(false);
+    }, 1000);
   }
 }
