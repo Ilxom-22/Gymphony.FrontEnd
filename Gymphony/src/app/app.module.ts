a8b10983b5bd1f4476a4e6ec9b6d1cc0bbb150c8
Ilxom <ilxomkarimjonov22@gmail.com>
2025-04-06 12:38:55 +0500

    Create config service for loading environment variable dynamically from config.json file

--- Gymphony/src/app/app.module.ts
+++ Gymphony/src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { ConfigService } from './config.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +16,9 @@ import { AuthInterceptor } from './core/interceptors/auth.interceptor';
 import { AuthResponseInterceptor } from './core/interceptors/auth-response.interceptor';
 import { SidenavComponent } from './core/components/sidenav/sidenav.component';
 
+export function initializeApp(configService: ConfigService) {
+  return () => configService.loadConfig();
+}
 
 @NgModule({
   declarations: [
@@ -42,6 +46,12 @@ import { SidenavComponent } from './core/components/sidenav/sidenav.component';
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthResponseInterceptor,
+      multi: true
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeApp,
+      deps: [ConfigService],
       multi: true
     }
   ],
